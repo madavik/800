0c92d30a541ae0f797007b5bbf0467d0e90fc606
madavik <joe.vgibson@gmail.com>
2016-07-07 23:45:01 -0600

    if classes have no prerequisites add them to the list

--- client/main.js
+++ client/main.js
@@ -3,25 +3,49 @@ import { ReactiveVar } from 'meteor/reactive-var';
 
 import './main.html';
 
-Template.hello.onCreated(function helloOnCreated() {
+Template.schedule.onCreated(function scheduleOnCreated() {
   // counter starts at 0
   this.counter = new ReactiveVar(0);
-  this.classes = new ReactiveVar([]);
+  this.classes = new ReactiveArray();
+  this.orderedClasses = new ReactiveArray();
 });
 
-Template.hello.helpers({
+Template.schedule.helpers({
   counter() {
     return Template.instance().counter.get();
   },
   classes() {
-    return Template.instance().classes.get();
+    return Template.instance().classes.list();
+  },
+  orderedClasses() {
+    return Template.instance().orderedClasses.list();
   },
 });
 
-Template.hello.events({
-  'click button'(event, instance) {
-  	console.log($('button'))
-    // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
+Template.schedule.events({
+  'click .add'(event, instance) {
+    var value = $('.class').val();
+  	if(value != '') {
+      instance.classes.push($('.class').val());
+    }
+  },
+  'click .order'(event, instance) {
+    var map = {};
+    var group = null;
+    var classes = instance.classes.list();
+    for (i = 0; i < classes.length; i++) {
+      group = classes[i].split(':');
+      if (group.length < 2) {
+        map[group[0]] = false;
+      }
+      else {
+        map[group[0]] = group[1];
+      }
+    }
+    for(var key in map){
+      if(!map[key]){
+        instance.orderedClasses.push(key);
+      }
+   }
   },
 });
