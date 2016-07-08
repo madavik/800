35d2e9123a90b21047969ffabfb424fcbf2395ae
madavik <joe.vgibson@gmail.com>
2016-07-08 01:36:27 -0600

    getting there

--- client/main.js
+++ client/main.js
@@ -30,21 +30,41 @@ Template.schedule.events({
     }
   },
   'click .order'(event, instance) {
+
     var map = {};
     var group = null;
     var classes = instance.classes.list();
+
+    function orderList(key) {
+      console.log(map);
+      if(!map[key].child){
+        instance.orderedClasses.push(key);
+        return;
+      }
+      else if(map[key].visited) {
+        return 'cycle';
+      }
+      map[key].visited = true;
+      orderList(map[key].child);
+    }
+
     for (i = 0; i < classes.length; i++) {
       group = classes[i].split(':');
+      map[group[0]] = {};
       if (group.length < 2) {
-        map[group[0]] = false;
+        map[group[0]].child = false;
       }
       else {
-        map[group[0]] = group[1];
+        map[group[0]].child = group[1].trim();
+        if(!map[map[group[0]].child]){
+          map[map[group[0]].child] = {};
+        }
       }
     }
     for(var key in map){
-      if(!map[key]){
-        instance.orderedClasses.push(key);
+      console.log('here');
+      if(instance.orderedClasses.indexOf() < 0){
+        orderList(key);
       }
    }
   },
