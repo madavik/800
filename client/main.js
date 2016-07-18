0a481242889542861b9cf77560cdc228e695dfde
madavik <joe.vgibson@gmail.com>
2016-07-17 23:52:17 -0600

    Added styling and started to refactor code

--- client/main.js
+++ client/main.js
@@ -6,7 +6,7 @@ import './main.html';
 Template.schedule.onCreated(function scheduleOnCreated() {
   // counter starts at 0
   this.counter = new ReactiveVar(0);
-  this.classes = new ReactiveArray();
+  this.classes = new ReactiveArray(['Introduction to Paper Airplanes:','Advanced Throwing Techniques: Introduction to Paper Airplanes', 'History of Cubicle Siege Engines: Rubber Band Catapults 101', 'Advanced Office Warfare: History of Cubicle Siege Engines', 'Rubber Band Catapults 101:', 'Paper Jet Engines: Introduction to Paper Airplanes']);
   this.orderedClasses = new ReactiveArray();
 });
 
@@ -26,45 +26,82 @@ Template.schedule.events({
   'click .add'(event, instance) {
     var value = $('.class').val();
   	if(value != '') {
-      instance.classes.push($('.class').val());
+      instance.classes.push($('.add-class-area').val());
     }
   },
   'click .order'(event, instance) {
 
-    var map = {};
+    var classTree = {};
     var group = null;
     var classes = instance.classes.list();
+    var finalReturn = 0;
+    var result = 0;
+
+    function addClass(key) {
+      if(instance.orderedClasses.indexOf(key) < 0){
+        instance.orderedClasses.push(key);
+      }
+    }
 
     function orderList(key) {
-      console.log(map);
-      if(!map[key].child){
-        instance.orderedClasses.push(key);
-        return;
+      if(!classTree[key].child || classTree[classTree[key].child].visited){
+        addClass(key);
+        if('parent' in classTree[key]){
+          return classTree[key].parent;
+        }
+        else {
+          return 1;
+        }
       }
-      else if(map[key].visited) {
-        return 'cycle';
+      else if(classTree[key].visited) {
+        return 0;
       }
-      map[key].visited = true;
-      orderList(map[key].child);
+      classTree[key].visited = true;
+      classTree[classTree[key].child].parent = key;
+      result = orderList(classTree[key].child);
+      if(result == 0){
+        return 0;
+      }
+      else if(result == 1) {
+        return 1;
+      }
+      else{
+        return addParent(result);
+      }
+    }
+    function addParent(key) {
+      addClass(key);
+      if('parent' in classTree[key]){
+        return classTree[key].parent;
+      }
+      else {
+        return 1;
+      }
     }
 
     for (i = 0; i < classes.length; i++) {
       group = classes[i].split(':');
-      map[group[0]] = {};
-      if (group.length < 2) {
-        map[group[0]].child = false;
+      classTree[group[0]] = {};
+      classTree[group[0]].visited = false;
+      if (group.length < 2 || group[1] == '') {
+        classTree[group[0]].child = false;
       }
       else {
-        map[group[0]].child = group[1].trim();
-        if(!map[map[group[0]].child]){
-          map[map[group[0]].child] = {};
+        classTree[group[0]].child = group[1].trim();
+        if(!classTree[classTree[group[0]].child]){
+          classTree[classTree[group[0]].child] = {};
+          classTree[classTree[group[0]].child].visited = false;
+          classTree[classTree[group[0]].child].child = false;
         }
       }
     }
-    for(var key in map){
-      console.log('here');
-      if(instance.orderedClasses.indexOf() < 0){
-        orderList(key);
+    for(var key in classTree){
+      if(instance.orderedClasses.indexOf(key) < 0){
+        finalReturn = orderList(key);
+        if(finalReturn == 0){
+          instance.orderedClasses.push('error');
+          break;
+        }
       }
    }
   },
